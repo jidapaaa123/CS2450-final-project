77ed1cb44c98f36a76b5bd9015dad19ec9d689ba
Jidapa Angsutti <jidapa.angsutti@students.snow.edu>
2025-04-09 11:26:21 -0600

    setup: tests, classes

---
+++ Console/UI.cs
@@ -0,0 +1,6 @@
+namespace Display;
+
+public class UI
+{
+
+}
---
+++ Logic.Tests/UnitTest2.cs
@@ -0,0 +1,10 @@
+namespace Logic.Tests;
+
+public class UnitTest2
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
