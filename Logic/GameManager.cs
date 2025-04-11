ae2175dfdd85e7fb74a9cfc0fd2e26918bdb4eb3
Jidapa Angsutti <jidapa.angsutti@students.snow.edu>
2025-04-11 11:23:43 -0600

    ASCII terrains: WIP

---
+++ Logic/GameManager.cs
@@ -0,0 +1,23 @@
+﻿namespace Logic;
+
+public class GameManager
+{
+    const int mapsAmount = 9;
+    const int width = 30;
+    const int height= 30;
+
+    List<int[,]> Maps = new List<int[,]>();
+
+    public GameManager()
+    {
+        // For each of the 9 maps, initialize arrays in Maps List
+        for (int i = 0; i < mapsAmount; i++)
+        {
+            Maps[i] = new int[height, width];
+        }
+
+
+    }
+
+}
+// <int[size.width, size.height]>
---
+++ Logic/Terrains.cs
@@ -0,0 +1,14 @@
+namespace Logic;
+
+/// <summary>
+/// Record of Terrain templates to choose from
+/// Returns 2D arrays of the Terrain template
+/// Template: Fixed items only, NO portals 
+/// </summary>
+public static class Terrains
+{
+    public static int[,] Home()
+    {
+        throw new NotImplementedException();
+    }
+}
