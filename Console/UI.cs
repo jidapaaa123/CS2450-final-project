acbda683835b50e8304d2233cee93c3d444cb5ef
Jidapa Angsutti <jidapa.angsutti@students.snow.edu>
2025-05-01 09:45:50 -0600

    deleted comments

--- Console/UI.cs
+++ Console/UI.cs
@@ -131,7 +131,6 @@ public class UI
             Console.Clear();
             Console.WriteLine("KITCHEN");
 
-            // This line below is so unreadable but I'm not gonna refactor it
             Console.WriteLine($"Cat's Request: {manager.RequestedFood}");
             Console.WriteLine($"Inventory: {Player.NumberOfItemsAsString(manager.Player.GetAmountsOfItems())}");
             Console.WriteLine("What'd you like to make?");
@@ -140,6 +139,7 @@ public class UI
             for (int i = 0; i < Kitchen.AllCraftables.Length; i++)
             {
                 Item item = Kitchen.AllCraftables[i];
+                // This line below is so unreadable but I'm not gonna refactor it
                 Console.WriteLine($"{i + 1}. {item.ToString().Replace('_', ' ')} ({Player.NumberOfItemsAsString(Player.OccurrencesOfItem(Kitchen.CraftableRecipes[item]))})");
             }
 
